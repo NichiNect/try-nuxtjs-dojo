efefd15f76ecb75dfaf583595bc5fc4efdac3f5f
NichiNect <yoniwidhi2@gmail.com>
2023-07-12 21:57:46 +0700

    Nuxt router link & minor layouts

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<style scoped>
+  .router-link-exact-active {
+    color: #eb2f9a;
+  }
+</style>
+
+<template>
+  <div>
+    <!-- Header -->
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/about">About</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/products">Products</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <!-- Content -->
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
